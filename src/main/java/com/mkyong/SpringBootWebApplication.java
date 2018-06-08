fb74e6950d8f883a85dfafbcee43d0abf3392153
akrasilnikov <meingipfel@mail.ru>
2018-06-08 11:59:05 +0300

    spring boot приложение. крутится на 8099 порту на сервере А Обращение к ней по  curl -F file=@"/opt/tomcat/conf/server.xml" http://172.16.1.16:8080/api/upload/ Мы говорим какой файл мы даём. и он летит на сервер А.

--- src/main/java/com/mkyong/SpringBootWebApplication.java
+++ src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -15,17 +15,18 @@ public class SpringBootWebApplication {
     //http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
     /*@Bean
     public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
-
         TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
-
         tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
             if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
                 //-1 means unlimited
                 ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
             }
         });
-
         return tomcat;
+
+
+ curl -F file=@"/opt/tomcat/conf/server.xml" http://172.16.1.16:8080/api/upload/
+
 
     }*/
 
