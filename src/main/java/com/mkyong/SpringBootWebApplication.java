73949f807d0e59a95fd16cd024bc5fb158ca2e3e
akrasilnikov <meingipfel@mail.ru>
2018-06-08 11:57:25 +0300

    Initial commit

---
+++ src/main/java/com/mkyong/SpringBootWebApplication.java
@@ -0,0 +1,32 @@
+package com.mkyong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+    private int maxUploadSizeInMb = 10 * 1024 * 1024; // 10 MB
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+    //http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
+    /*@Bean
+    public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+
+        tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+                //-1 means unlimited
+                ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+            }
+        });
+
+        return tomcat;
+
+    }*/
+
+}
